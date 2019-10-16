9c27b15bbb12aa761e519f4c50d1aebe7e8c82f0
Stanoque <stanoquedzhumpsh@gmail.com>
2019-10-16 22:57:15 +0300

    webpack is adjusted

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 const path = require('path');
 const html_webpack = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-    entry: 'index.js',
-    output: {
-        path: PATHS.build,
-        filename: '[name].js'
-    },
-    plugins: [
-        new html_webpack({
-            title: 'FSD Second Step'
+const common = merge ([
+    { 
+        entry: './index.js',
+        output: {
+            path: PATHS.build,
+            filename: '[name].js'
+        },
+        plugins: [
+            new html_webpack({
+            template: './index.pug',
         })
-    ]
+        ]
+    },
+   
+    pug()
+])
+
+
+module.exports = function(env) {
+    if (env === 'production'){
+        return common;
+    }
+    if (env == 'development'){
+        return merge([
+            common,
+            devserver()
+        ])
+    }
 };
